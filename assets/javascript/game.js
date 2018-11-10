c4330b3bf3ad019a3f8142b2db852afe48e45fcc
Kakalak <43103025+cdgillis@users.noreply.github.com>
2018-11-10 01:39:43 -0500

    more changes to upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,22 @@
 var wordBank = ["yoshi", "mario", "luigi", "peach", "toad", "bowser", "wario", "waluigi"];
 var currentWord = ""; // Stores the word we want the player to guess
-var userGuess = "";
-var remainingLetters = ""; // GLOBAL???
 var answerArray = []; // Stores the answer but begins with _
-var alreadyGuessedArr = [];
+var alreadyGuessedArr = []; // Space to store letter guesses not matched correctly
 var wins = 0;
+
+var x = document.getElementById("win-audio"); 
+
+function playAudio() { 
+    x.play(); 
+} 
+
+function pauseAudio() { 
+    x.pause(); 
+}
 
 window.onload = function startup() {
 
+    $("#game-won").css("display", "none");
     // Select random word
     currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 
@@ -21,40 +30,58 @@ window.onload = function startup() {
 }
 
 var i = 0;
+var numberGuesses = document.getElementById("guesses-left").innerHTML = 12;
 
 // Words Guessed
 document.onkeyup = function (event) {
 
-
     /*// Process user's letter input
     for (var i = 0; i < currentWord.length; i++) {
-
         eachChar.push(currentWord.charAt(i));
         hiddenChar.push(" _ ");
-
     }*/
+
     var letter = event.key;
     console.log(letter);
-    //letter = letter.toUpperCase();
 
+    var found = false; //I made this variable
+    // Set number of guesses
+    
 
-    //var i = 0
+    //Look for letter in word
     for (i = 0; i < currentWord.length; i++) {
         if (currentWord[i] == letter) {
+            // Found the letter
+            found = true;
             answerArray[i] = letter;
             document.getElementById("answer").innerHTML = answerArray.join(" ");
         }
+        numberGuesses--;
     }
-
-    /*for (i = 0; i < currentWord.length; i++) {
-        if (currentWord[i] !== letter) {
-            document.getElementById("userGuess").append(letter + " ");
-        }
-    }*/
+    // Finished checking the word for the letter
+    if (!found) {
+        alreadyGuessedArr.push(letter);
+        document.getElementById("user-guess").innerHTML = alreadyGuessedArr.join(" ");
+        numberGuesses--;
+    }
+   
+}
+// Update the game for remaining unknowns
+var remaining_letters = answerArray.length;
+// recount the remaining letters
+for (i = 0; i < answerArray.length; i++) {
+    if (answerArray[i] !== '_') {
+        remaining_letters -= 1;
+    }
 }
 
-/*// Set number of guesses
-    numberGuesses = document.getElementById("guessesLeft").innerHTML = 12;
+// if no remaining letters, hurray, you won
+if (remaining_letters == 0) {
+    playAudio();
+    $("#game-won").css("display");
+}
+
+/*
     
     // Make a counter for guesses remaining
     numberGuesses--;
