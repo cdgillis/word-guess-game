9d0ef0048d8441a21d74cd9de1e6260169daa994
Kakalak <43103025+cdgillis@users.noreply.github.com>
2018-11-26 18:33:21 -0500

    updated game upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,18 +3,27 @@ var currentWord = ""; // Stores the word we want the player to guess
 var answerArray = []; // Stores the answer but begins with _
 var alreadyGuessedArr = []; // Space to store letter guesses not matched correctly
 var wins = 0;
-var x = document.getElementById("win-audio");
+var x = document.getElementById("correct-audio");
 var y = document.getElementById("wrong-audio");
+var won = document.getElementById("won-audio");
+var lost = document.getElementById("lost-audio");
+// var vid = document.getElementById("myVideo"); 
 
 function playAudio() {
     x.play();
     y.play();
+    won.play();
+    lost.play();
 }
 
-window.onload = function startup() {
+function startup() {
 
-    $("#game-won").css("display", "none");
+    $("#game-won").css("display", "none"); //Need to create an on.click function with a button
+    $("#game-lost").css("display", "none");
+    $("#container").css("display", "block");
     // Select random word
+    answerArray = [];
+    currentWord = "";
     currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 
     for (var i = 0; i < currentWord.length; i++) {
@@ -23,15 +32,41 @@ window.onload = function startup() {
 
     document.getElementById("answer").innerHTML = answerArray.join(" ")
     console.log(currentWord);
-
 }
 
-var i = 0;
-var numberGuesses = document.getElementById("guesses-left").innerHTML = 12;
+function resetGame() {
+    numberGuesses = 12;
+    document.getElementById("guesses-left").innerHTML = numberGuesses;
+    answerArray = [];
+    alreadyGuessedArr = [];
+    document.getElementById("user-guess").innerHTML = alreadyGuessedArr;
+    startup();
+}
 
+function wonGame() {
+    console.log("Level Complete!");
+    $("#container").css("display", "none");
+    $("#game-won").css("display", "block");
+    won.play();
+}
+
+function lostGame() {
+    console.log("GAME OVER");
+    $("#container").css("display", "none");
+    $("#game-lost").css("display", "block");
+    lost.play();
+}
+
+window.onload = startup;
+var i = 0;
+var numberGuesses = 12;
+console.log(numberGuesses);
+document.getElementById("guesses-left").innerHTML = numberGuesses;
 // Words Guessed
 document.onkeyup = function (event) {
     numberGuesses -= 1;
+    document.getElementById("guesses-left").innerHTML = numberGuesses;
+    console.log(numberGuesses);
     var letter = event.key;
     console.log(letter);
 
@@ -46,7 +81,7 @@ document.onkeyup = function (event) {
             found = true;
             answerArray[i] = letter;
             document.getElementById("answer").innerHTML = answerArray.join(" ");
-            numberGuesses--;
+            // numberGuesses--;
         }
 
     }
@@ -55,18 +90,24 @@ document.onkeyup = function (event) {
         y.play();
         alreadyGuessedArr.push(letter);
         document.getElementById("user-guess").innerHTML = alreadyGuessedArr.join(" ");
-        numberGuesses--;
+        // numberGuesses--;
 
     }
-    // $("#game-won").css("display");
-    // Make a counter for guesses remaining
+    if ((numberGuesses > 0) && (currentWord == answerArray.join(""))) {
+        wins++;
+        console.log("You have completed " + wins + " levels");
+        document.getElementById("win-counter").innerHTML = wins;
+        wonGame();
+        
+    } else if (numberGuesses == 0){ 
+        lostGame();
+    }
    
-    document.getElementById("guessesLeft").innerHTML = numberGuesses--;
-
-
 }
 
-
+// $('#resetBtn').click(function(){
+//     //Some code
+// });
 
 /*// Update the game for remaining unknowns
 var remaining_letters = answerArray.length;
@@ -83,8 +124,6 @@ if (remaining_letters == 0) {
     $("#game-won").css("display");
 }
 */
-
-
 
 /*
     
